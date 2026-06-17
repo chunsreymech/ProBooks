17de67d4e618e577df83912616a0262dd4178819
Chun Sreymech <chunsreymech92@gmail.com>
2026-06-17 19:09:44 +0700

    Update Book.cs

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,8 +1,10 @@
-namespace BookApi.Models {
-    public class Book {
+namespace BookApi.Models
+{
+    public class Book
+    {
         public int Id { get; set; }
         public string Title { get; set; } = "";
         public string Author { get; set; } = "";
-        public decimal Price { get; set; } = 0.00;
+        public decimal Price { get; set; } = 0;
     }
 }
